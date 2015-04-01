af4851714dd8d28e6015d79291aee1b4a966d1e7
m1k3s <pbrane0@gmail.com>
2015-04-01 12:42:20 -0400

    Adding settings to app

---
+++ app/src/main/java/com/pbrane/mike/ipv4subnet/UserSettingsActivity.java
@@ -0,0 +1,15 @@
+package com.pbrane.mike.ipv4subnet;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+
+public class UserSettingsActivity extends PreferenceActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_main);
+
+	}
+}
